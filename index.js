4ed601ca1f09a4e240deea8dc16d7fa4184a2f1a
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-04-28 01:14:41 -0500

    completion of array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter((strings) => strings.toLowerCase().includes(string.toLowerCase()))
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter((strings) => strings.charAt(0) === string.charAt(0))
+}
+
+function matchName(driver, string){
+    return driver.filter((driverInfo) => driverInfo.name === string)
+}
